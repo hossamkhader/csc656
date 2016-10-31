4d039d5316105c72bbdd8c318405d43284a35468
Michael Branon <michaelabranon@gmail.com>
2016-10-31 10:30:47 -0400

    added custom exceptions to help in handling type error checking

---
+++ src/csc656/DegreeMismatch.java
@@ -0,0 +1,19 @@
+package csc656;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Michael Branon
+ */
+public class DegreeMismatch extends Exception {
+
+
+    public DegreeMismatch(String msg) {
+        super(msg);
+    }
+}
---
+++ src/csc656/SubTypeNotFound.java
@@ -0,0 +1,18 @@
+package csc656;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Michael Branon
+ */
+public class SubTypeNotFound extends Exception {
+
+    public SubTypeNotFound(String msg) {
+        super(msg);
+    }
+}
