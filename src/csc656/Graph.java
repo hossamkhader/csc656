ba0873359c3aa21793ed1dbc71c2ab3942bb0d34
yevot <jonathon.tovey@gmail.com>
2016-11-14 09:45:01 -0500

    removing addition of seed edge to graph compression

--- src/csc656/Graph.java
+++ src/csc656/Graph.java
@@ -253,18 +253,22 @@ public class Graph {
             System.out.println("Edge " + sw + " removed");
         }
         
-        //add seed edge
-        Vertex sOrigin = graphCopy.getVertex(seed.substring(0, n-1));
-	// debug // System.out.println(sOrigin.getLabel());
-        Vertex sDestination = 
-                this.getVertex(seed.substring(seed.length() - n + 1));
-	// debug // System.out.println(sDestination.getLabel());
-
-	// debug //
-	System.out.print("Adding seed edge " + seed);
-	System.out.println(" from " + sOrigin.getLabel() + " to " + sDestination.getLabel());
-
-        graphCopy.addEdge(sOrigin, sDestination, seed);
+        
+// skip adding seed edge until later        
+//        //add seed edge
+//        Vertex sOrigin = graphCopy.getVertex(seed.substring(0, n-1));
+//	// debug // System.out.println(sOrigin.getLabel());
+//        Vertex sDestination = 
+//                this.getVertex(seed.substring(seed.length() - n + 1));
+//	// debug // System.out.println(sDestination.getLabel());
+//
+//	// debug //
+//	System.out.print("Adding seed edge " + seed);
+//	System.out.println(" from " + sOrigin.getLabel() + " to " + sDestination.getLabel());
+//
+//        graphCopy.addEdge(sOrigin, sDestination, seed);
+        
+        // remove any (0, 0) vertices
         
         Graph graphCopyIter = new Graph(graphCopy);
         for (String vStr : graphCopyIter.vertexKeys()) {
